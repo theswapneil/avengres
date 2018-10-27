c509061671077841bfa21a457d70e96314a6f1d1
Swapy <swapnil.tale.compu@gmail.com>
2018-10-27 09:02:47 +0530

    mock  heroes changes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Captain America' },
+  { id: 12, name: 'Iron man' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Thor' },
+  { id: 15, name: 'Black Widow' },
+  { id: 16, name: 'Spider man' },
+  { id: 17, name: 'Doctor Strange' },
+  { id: 18, name: 'Black Panther' }
+];
